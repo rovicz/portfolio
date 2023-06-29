c6b4fa3bf891af40125b432931729a11110807ff
Victor Fernandes <roviczzf@gmail.com>
2023-06-29 18:05:16 -0300

    js organizer

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,3 @@
-function initAnimacaoScroll() {
-  const sections = document.querySelectorAll(".js-scroll");
-  if (sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
-
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = sectionTop - windowMetade < 0;
-        if (isSectionVisible) section.classList.add("ativo");
-        else section.classList.remove("ativo");
-      });
-    }
-
-    animaScroll();
-
-    window.addEventListener("scroll", animaScroll);
-  }
-}
+import initAnimacaoScroll from "./modules/scrollAnimation.js";
 
 initAnimacaoScroll();
