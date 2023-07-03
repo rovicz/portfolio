b0eca6c7f684c690f81c334dbaa6853f7cd08b6e
Victor Fernandes <roviczzf@gmail.com>
2023-07-03 17:30:06 -0300

    Scroll to Top Button

---
+++ src/js/modules/scrollTop.js
@@ -0,0 +1,25 @@
+export default function initScrollTop() {
+  const button = document.getElementById("topButton");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  }
+
+  function topScroll() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  button.addEventListener("click", topScroll);
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 import initCopyButton from "./modules/copyButton.js";
 import initAnimacaoScroll from "./modules/scrollAnimation.js";
+import initScrollTop from "./modules/scrollTop.js";
 
 initAnimacaoScroll();
 initCopyButton();
+initScrollTop();
